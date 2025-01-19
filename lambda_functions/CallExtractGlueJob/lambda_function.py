642ef46243bd20126f2f189aab38b44911915733
Gabriel Guizardi <gabriel.guizardi.dev@gmail.com>
2025-01-19 09:35:18 -0300

    Lambda para realizar a chamada dos jobs

--- lambda_functions/CallExtractGlueJob/lambda_function.py
+++ lambda_functions/CallExtractGlueJob/lambda_function.py
@@ -1,8 +1,21 @@
 import json
+import boto3
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    glue = boto3.client('glue')
+    try:
+        response = glue.start_job_run(JobName='extract')
+        
+        job_run_id = response['JobRunId']
+        print(f"Started Glue job 'extract' with JobRunId: {job_run_id}")
+        
+        return {
+            'statusCode': 200,
+            'body': f"Started Glue job 'extract' with JobRunId: {job_run_id}"
+        }
+    except Exception as e:
+        print(f"Error starting Glue job: {e}")
+        return {
+            'statusCode': 500,
+            'body': f"Error starting Glue job: {e}"
+        }
